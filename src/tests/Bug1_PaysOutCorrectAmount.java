30eccf8ceb0ab289d026c36a766c56589d295d60
Lecks <leckschester@hotmail.com>
2016-10-01 19:22:43 +1000

    Tests for resolving Bug 1

--- src/tests/Bug1_PaysOutCorrectAmount.java
+++ src/tests/Bug1_PaysOutCorrectAmount.java
@@ -58,13 +58,46 @@ public class Bug1_PaysOutCorrectAmount {
 		Dice die2 = getMockDice(pick);
 		Dice die3 = getMockDice(pick);
 		
-		
 		Game game = new Game(die1, die2, die3);
 		Player player = new Player("Pete", 100);
 		
 		game.playRound(player, pick, 10);
 		
 		assertEquals(130, player.getBalance());
+	}
+	
+	@Test
+	public void testGetBalance() {
+		Player player = new Player("Pete", 100);
+		assertEquals(100, player.getBalance());
+	}
+	
+	@Test
+	public void testTakeBet() {
+		Player player = new Player("Pete", 100);
+		player.takeBet(10);
+		assertEquals(90, player.getBalance());
+	}
+	
+	@Test
+	public void testReceiveWinnings() {
+		Player player = new Player("Pete", 100);
+		player.receiveWinnings(10);
+		assertEquals(110, player.getBalance());
+	}
+	
+	@Test
+	public void testPlayRound_PlayerGetsCorrectWinnings_NoMatches() {
+		DiceValue pick = DiceValue.ANCHOR;
+		Dice die1 = getMockDice(DiceValue.CLUB);
+		Dice die2 = getMockDice(DiceValue.CLUB);
+		Dice die3 = getMockDice(DiceValue.CLUB);
+		Game game = new Game(die1, die2, die3);
+		Player player = new Player("Pete", 100);
+		
+		game.playRound(player, pick, 10);
+		
+		assertEquals(90, player.getBalance());
 	}
 	
 	private static Dice getMockDice(DiceValue value)
