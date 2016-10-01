5b10e5beb19e41120876325ecb3b31cb647c5236
Lecks <leckschester@hotmail.com>
2016-10-01 19:23:28 +1000

    Bug 1 fix

--- src/crown_and_anchor/Game.java
+++ src/crown_and_anchor/Game.java
@@ -40,9 +40,10 @@ public class Game {
 			}
 		}
 		
-		int winnings = matches * bet;
+		int winnings = 0;
 
-		if (matches > 0) {			
+		if (matches > 0) {	
+			winnings = (matches+1) * bet;
 			player.receiveWinnings(winnings);
 		}
         return winnings;		
